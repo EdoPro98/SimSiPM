104191c1733fd4284563e90149b2ea577f9b8210
EdoPro98 <edoardo.proserpio@gmail.com>
2021-04-13 23:23:57 +0200

    Modified random gaussian. Borrowed Cern ROOT one

--- src/SiPMRandom.cpp
+++ src/SiPMRandom.cpp
@@ -13,14 +13,12 @@ void Xorshift256plus::seed() {
   this->operator()();
 }
 
-
 void Xorshift256plus::seed(const uint64_t aseed) {
   s[0] = aseed;
   s[1] = aseed + 1;
   s[2] = aseed + 2;
   s[3] = aseed + 3;
 }
-
 
 void Xorshift256plus::jump() {
   static const uint64_t JUMP[] = {0x180ec6d33cfd0aba, 0xd5a61266f0c9392c, 0xa9582618e03fc9aa, 0x39abdc4529b1661c};
@@ -42,7 +40,7 @@ void Xorshift256plus::jump() {
   s[2] = s2;
   s[3] = s3;
 }
-}  // namespace SiPMRng
+} // namespace SiPMRng
 
 // SCALAR //
 
@@ -50,7 +48,7 @@ void Xorshift256plus::jump() {
  * @param mu Mean value of the poisson distribution
  */
 uint32_t SiPMRandom::randPoisson(const double mu) {
-  if (mu == 0) {
+  if (mu <= 0) {
     return 0;
   }
   const double q = exp(-mu);
@@ -64,12 +62,10 @@ uint32_t SiPMRandom::randPoisson(const double mu) {
   return out;
 }
 
-
 /** @brief Returns a value from a exponential distribution given its mean value.
  * @param mu Mean value of the exponential distribution
  */
 double SiPMRandom::randExponential(const double mu) { return -log(Rand()) * mu; }
-
 
 /** @brief Returns a value from a gaussian distribution given its mean value and
  * sigma.
@@ -79,26 +75,95 @@ double SiPMRandom::randExponential(const double mu) { return -log(Rand()) * mu; 
  * @param sigma Standard deviation of the gaussian distribution
  */
 double SiPMRandom::randGaussian(const double mu, const double sigma) {
-  static double spare;
-  static bool hasSpare = false;
+  constexpr double kC1 = 1.448242853;
+  constexpr double kC2 = 3.307147487;
+  constexpr double kC3 = 1.46754004;
+  constexpr double kD1 = 1.036467755;
+  constexpr double kD2 = 5.295844968;
+  constexpr double kD3 = 3.631288474;
+  constexpr double kHm = 0.483941449;
+  constexpr double kZm = 0.107981933;
+  constexpr double kHp = 4.132731354;
+  constexpr double kZp = 18.52161694;
+  constexpr double kPhln = 0.4515827053;
+  constexpr double kHm1 = 0.516058551;
+  constexpr double kHp1 = 3.132731354;
+  constexpr double kHzm = 0.375959516;
+  constexpr double kHzmp = 0.591923442;
+  /*zhm 0.967882898*/
 
-  if (hasSpare) {
-    hasSpare = false;
-    return spare * sigma + mu;
-  } else {
-    double u, v, s;
-    do {
-      u = Rand() * 2.0 - 1.0;
-      v = Rand() * 2.0 - 1.0;
-      s = (u * u) + (v * v);
-    } while (s >= 1.0 || s == 0.0);
-    s = sqrt(log(s) * (-2. / s));
-    spare = v * s;
-    hasSpare = true;
-    return u * s * sigma + mu;
-  }
+  constexpr double kAs = 0.8853395638;
+  constexpr double kBs = 0.2452635696;
+  constexpr double kCs = 0.2770276848;
+  constexpr double kB = 0.5029324303;
+  constexpr double kX0 = 0.4571828819;
+  constexpr double kYm = 0.187308492;
+  constexpr double kS = 0.7270572718;
+  constexpr double kT = 0.03895759111;
+
+  double result;
+  double rn, x, y, z;
+
+  do {
+    y = Rand();
+
+    if (y > kHm1) {
+      result = kHp * y - kHp1;
+      break;
+    }
+
+    else if (y < kZm) {
+      rn = kZp * y - 1;
+      result = (rn > 0) ? (1 + rn) : (-1 + rn);
+      break;
+    }
+
+    else if (y < kHm) {
+      rn = Rand();
+      rn = rn - 1 + rn;
+      z = (rn > 0) ? 2 - rn : -2 - rn;
+      if ((kC1 - y) * (kC3 + abs(z)) < kC2) {
+        result = z;
+        break;
+      } else {
+        x = rn * rn;
+        if ((y + kD1) * (kD3 + x) < kD2) {
+          result = rn;
+          break;
+        } else if (kHzmp - y < exp(-(z * z + kPhln) / 2)) {
+          result = z;
+          break;
+        } else if (y + kHzm < exp(-(x + kPhln) / 2)) {
+          result = rn;
+          break;
+        }
+      }
+    }
+
+    while (1) {
+      x = Rand();
+      y = kYm * Rand();
+      z = kX0 - kS * x - y;
+      if (z > 0)
+        rn = 2 + y / x;
+      else {
+        x = 1 - x;
+        y = kYm - y;
+        rn = -(2 + y / x);
+      }
+      if ((y - kAs + x) * (kCs + x) + kBs < 0) {
+        result = rn;
+        break;
+      } else if (y < x + kT)
+        if (rn * rn < 4 * (kB - log(x))) {
+          result = rn;
+          break;
+        }
+    }
+  } while (0);
+
+  return mu + sigma * result;
 }
-
 
 /** @brief Returns a random integer in range [0,max]
  * @param max Maximum value of integer to generate
@@ -122,7 +187,6 @@ std::vector<double> SiPMRandom::Rand(const uint32_t n) {
   return out;
 }
 
-
 /**
  * @param mu Mean value of the gaussuan
  * @param sigma Standard deviation value of the gaussuan
@@ -133,8 +197,7 @@ std::vector<double> SiPMRandom::randGaussian(const double mu, const double sigma
     return {};
   }
   alignas(64) double out[n];
-  alignas(64) double ss[n];
-  alignas(64) double uu[n];
+  alignas(64) double norm[n];
 
   for (uint32_t i = 0; i < n - 1; i += 2) {
     double s, u, v;
@@ -144,18 +207,16 @@ std::vector<double> SiPMRandom::randGaussian(const double mu, const double sigma
       s = (u * u) + (v * v);
     } while (s >= 1.0 || s == 0.0);
 
-    ss[i] = sqrt(log(s) * (-2. / s));
-    ss[i + 1] = ss[i];
-    uu[i] = u;
-    uu[i + 1] = v;
+    s = sqrt(log(s) * (-2. / s));
+    norm[i] = s * u;
+    norm[i + 1] = s * v;
   }
   for (uint32_t i = 0; i < n; ++i) {
-    out[i] = ss[i] * uu[i] * sigma + mu;
+    out[i] = norm[i] * sigma + mu;
   }
   std::vector<double> res(out, out + n);
   return res;
 }
-
 
 /**
  * @param max Max value to generate
@@ -181,4 +242,4 @@ std::vector<double> SiPMRandom::randExponential(const double mu, const uint32_t 
   }
   return out;
 }
-}  // namespace sipm
+} // namespace sipm
