0b26a6e10a0f8f30d904e8c8c9c905d6db8f8f4f
EdoPro98 <edoardo.proserpio@gmail.com>
2022-04-06 17:46:38 +0200

    Removed unused method. setProperty is now case insensitive

--- src/SiPMProperties.cpp
+++ src/SiPMProperties.cpp
@@ -1,9 +1,12 @@
 #include "SiPMProperties.h"
+#include <algorithm>
+#include <cctype>
 
 namespace sipm {
 
 // Getters
 uint32_t SiPMProperties::nCells() const {
+  // m_SideCells and m_Ncells are cached
   if ((m_SideCells == 0) || (m_Ncells == 0)) {
     m_SideCells = 1000 * m_Size / m_Pitch;
     m_Ncells = m_SideCells * m_SideCells;
@@ -12,6 +15,7 @@ uint32_t SiPMProperties::nCells() const {
 }
 
 uint32_t SiPMProperties::nSideCells() const {
+  // m_SideCells and m_Ncells are cached
   if ((m_SideCells == 0) || (m_Ncells == 0)) {
     m_SideCells = 1000 * m_Size / m_Pitch;
     m_Ncells = m_SideCells * m_SideCells;
@@ -20,6 +24,7 @@ uint32_t SiPMProperties::nSideCells() const {
 }
 
 uint32_t SiPMProperties::nSignalPoints() const {
+  // m_Signalpoints is cached
   if (m_SignalPoints == 0) {
     m_SignalPoints = m_SignalLength / m_Sampling;
   }
@@ -27,6 +32,7 @@ uint32_t SiPMProperties::nSignalPoints() const {
 }
 
 double SiPMProperties::snrLinear() const {
+  // m_SnrLinear is cached
   if (m_SnrLinear == 0) {
     m_SnrLinear = pow(10, -m_SnrdB / 20);
   }
@@ -34,111 +40,67 @@ double SiPMProperties::snrLinear() const {
 }
 
 // Setters
-void SiPMProperties::setProperty(const std::string& aProp, const double aPropValue) {
-  if (aProp == "Size") {
-    setSize(aPropValue);
-    m_SideCells = 1000 * m_Size / m_Pitch;
-    m_Ncells = m_SideCells * m_SideCells;
-  } else if (aProp == "Pitch") {
-    setPitch(aPropValue);
-    m_SideCells = 1000 * m_Size / m_Pitch;
-    m_Ncells = m_SideCells * m_SideCells;
-  } else if (aProp == "Sampling") {
-    setSampling(aPropValue);
-    m_SignalPoints = m_SignalLength / m_Sampling;
-  } else if (aProp == "CellRecovery") {
-    setRecoveryTime(aPropValue);
-  } else if (aProp == "SignalLength") {
-    setSignalLength(aPropValue);
-    m_SignalPoints = m_SignalLength / m_Sampling;
-  } else if (aProp == "RiseTime") {
-    setRiseTime(aPropValue);
-  } else if (aProp == "FallTimeFast") {
-    setFallTimeFast(aPropValue);
-  } else if (aProp == "FallTimeSlow") {
-    setFallTimeSlow(aPropValue);
-  } else if (aProp == "SlowComponentFraction") {
-    setSlowComponentFraction(aPropValue);
-  } else if (aProp == "RecoveryTime") {
-    setRecoveryTime(aPropValue);
-  } else if (aProp == "TauApFast") {
-    setTauApFastComponent(aPropValue);
-  } else if (aProp == "TauApSlow") {
-    setTauApSlowComponent(aPropValue);
-  } else if (aProp == "Ccgv") {
-    setCcgv(aPropValue);
-  } else if (aProp == "Snr") {
-    setSnr(aPropValue);
-    m_SnrLinear = pow(10, -m_SnrdB / 20);
-  } else if (aProp == "Pde") {
-    setPde(aPropValue);
-  } else if (aProp == "Dcr") {
-    setDcr(aPropValue);
-  } else if (aProp == "Xt") {
-    setXt(aPropValue);
-  } else if (aProp == "DXt") {
-    setDXt(aPropValue);
-  } else if (aProp == "Ap") {
-    setAp(aPropValue);
+void SiPMProperties::setProperty(const std::string& prop, const double val) {
+  // Make prop lowercase to avoid case-sensitive property mismatch
+  std::string aProp(prop);
+  std::transform(prop.begin(), prop.end(), aProp.begin(), [](char c) -> char { return std::tolower(c); });
+
+  if (aProp == "size") {
+    setSize(val);
+  } else if (aProp == "pitch") {
+    setPitch(val);
+  } else if (aProp == "sampling") {
+    setSampling(val);
+  } else if (aProp == "cellrecovery") {
+    setRecoveryTime(val);
+  } else if (aProp == "signallength") {
+    setSignalLength(val);
+  } else if (aProp == "risetime") {
+    setRiseTime(val);
+  } else if (aProp == "falltimefast") {
+    setFallTimeFast(val);
+  } else if (aProp == "falltimeslow") {
+    setFallTimeSlow(val);
+  } else if (aProp == "slowcomponentfraction") {
+    setSlowComponentFraction(val);
+  } else if (aProp == "recoverytime") {
+    setRecoveryTime(val);
+  } else if (aProp == "tauapfast") {
+    setTauApFastComponent(val);
+  } else if (aProp == "tauapslow") {
+    setTauApSlowComponent(val);
+  } else if (aProp == "ccgv") {
+    setCcgv(val);
+  } else if (aProp == "snr") {
+    setSnr(val);
+  } else if (aProp == "pde") {
+    setPde(val);
+  } else if (aProp == "dcr") {
+    setDcr(val);
+  } else if (aProp == "xt") {
+    setXt(val);
+  } else if (aProp == "dxt") {
+    setDXt(val);
+  } else if (aProp == "ap") {
+    setAp(val);
   } else {
-    std::cerr << "Property: " << aProp << " not found! \n";
+    std::cerr << "Property: " << aProp << " not found!" << std::endl;
   }
 }
 
-void SiPMProperties::setSampling(const double aSampling) {
-  m_Sampling = aSampling;
-  m_SignalPoints = static_cast<uint32_t>(m_SignalLength / aSampling);
-}
-
-void SiPMProperties::setPdeSpectrum(const std::map<double, double>& x) {
-  static constexpr uint32_t N = 25;
-
-  std::map<double,double> interpolatedSpectrum = x;
-
-  const double xmin = interpolatedSpectrum.begin()->first;
-  const double xmax = interpolatedSpectrum.rbegin()->first;
-  const double dx = (xmax - xmin) / N;
-  for(uint32_t i=0; i<N; ++i){
-    const double newx = xmin + i*dx;  // Current x value to interpolate
-    if (interpolatedSpectrum.count(newx) > 0){ // Skip in case value exist
-      continue;
-    }
-    auto it1 = x.upper_bound(newx);   // Iterator to first value greater than newx
-    if (it1 == x.end()) { // If last value get back by one
-      --it1;
-    }
-    if (it1 == x.begin()) { // If first value go ahead by one
-      ++it1;
-    }
-    auto it0 = it1;
-    --it0;  // It0 is iterator before it1 (last value less than newx)
-    const double x0 = it0->first;
-    const double x1 = it1->first;
-    double y0 = it0->second;
-    double y1 = it1->second;
-    const double logNewy = (std::log(y0)*std::log(x1/newx)+std::log(y1)*std::log(newx/x0))/ std::log(x1/x0);
-    double newy = std::exp(logNewy);
-    if (newy == 0){ // Newy cannot be 0 becouse we are avoiding boundary conditions
-      // Linear interpolation
-      const double m = (y1-y0)/(x1-x0);
-      const double q = y0 - m*x0;
-      newy = m*newx + q;
-    }
-    interpolatedSpectrum.emplace(newx,newy);
-  }
-
-  m_PdeSpectrum = interpolatedSpectrum;
-  m_HasPde = PdeType::kSpectrumPde;
+void SiPMProperties::setSampling(const double val) {
+  m_Sampling = val;
+  m_SignalPoints = static_cast<uint32_t>(m_SignalLength / m_SignalPoints);
 }
 
 void SiPMProperties::setPdeSpectrum(const std::vector<double>& wav, const std::vector<double>& pde) {
   static constexpr uint32_t N = 25;
 
-  std::map<double,double> interpolatedSpectrum;
-  std::map<double,double> x;
+  std::map<double, double> interpolatedSpectrum;
+  std::map<double, double> x;
 
   for (uint32_t i = 0, n = wav.size(); i < n; ++i) {
-    x.emplace(wav[i],pde[i]);
+    x.emplace(wav[i], pde[i]);
   }
 
   interpolatedSpectrum = x;
@@ -146,9 +108,9 @@ void SiPMProperties::setPdeSpectrum(const std::vector<double>& wav, const std::v
   const double xmin = x.begin()->first;
   const double xmax = x.rbegin()->first;
   const double dx = (xmax - xmin) / 25;
-  for(uint32_t i=0; i<N; ++i){
-    const double newx = xmin + i*dx;
-    if (interpolatedSpectrum.count(newx) > 0){ // Skip in case value exist
+  for (uint32_t i = 0; i < N; ++i) {
+    const double newx = xmin + i * dx;
+    if (interpolatedSpectrum.count(newx) > 0) { // Skip in case value exist
       continue;
     }
     auto it1 = x.upper_bound(newx);
@@ -165,15 +127,15 @@ void SiPMProperties::setPdeSpectrum(const std::vector<double>& wav, const std::v
     const double x1 = it1->first;
     const double y0 = it0->second;
     const double y1 = it1->second;
-    const double logNewy = (std::log(y0)*std::log(x1/newx)+std::log(y1)*std::log(newx/x0))/ std::log(x1/x0);
+    const double logNewy = (log(y0) * log(x1 / newx) + log(y1) * log(newx / x0)) / log(x1 / x0);
     double newy = std::exp(logNewy);
-    if (newy == 0){ // Newy cannot be 0 becouse we are avoiding boundary conditions
+    if (newy == 0) { // Newy cannot be 0 becouse we are avoiding boundary conditions
       // Linear interpolation
-      const double m = (y1-y0)/(x1-x0);
-      const double q = y0 - m*x0;
-      newy = m*newx + q;
+      const double m = (y1 - y0) / (x1 - x0);
+      const double q = y0 - m * x0;
+      newy = m * newx + q;
     }
-    interpolatedSpectrum.emplace(newx,newy);
+    interpolatedSpectrum.emplace(newx, newy);
   }
 
   m_PdeSpectrum = interpolatedSpectrum;
@@ -185,10 +147,13 @@ void SiPMProperties::readSettings(std::string& fname) {
   if (cFile.is_open()) {
     std::string line;
     while (getline(cFile, line)) {
+      // Remove spaces
       line.erase(std::remove_if(line.begin(), line.end(), isspace), line.end());
+      // Ignore lines starting with # or /
       if (line[0] == '#' || line[0] == '/' || line.empty()) {
         continue;
       }
+      // Get prop - value
       auto delimiterPos = line.find("=");
       auto varName = line.substr(0, delimiterPos);
       auto varValue = line.substr(delimiterPos + 1);
@@ -196,14 +161,14 @@ void SiPMProperties::readSettings(std::string& fname) {
       setProperty(varName, std::stod(varValue));
     }
   } else {
-    std::cerr << "Could not open config file for reading.\n";
+    std::cerr << "Could not open " << fname << " for reading!" << std::endl;
   }
 }
 
-std::ostream& operator<< (std::ostream& out, const SiPMProperties& obj){
+std::ostream& operator<<(std::ostream& out, const SiPMProperties& obj) {
   out << std::setprecision(2) << std::fixed;
   out << "===> SiPM Properties <===" << '\n';
-  out << "Address: "<<std::addressof(obj) << "\n";
+  out << "Address: " << std::addressof(obj) << "\n";
   out << "Size: " << obj.m_Size << " mm\n";
   out << "Pitch: " << obj.m_Pitch << " um\n";
   out << "Number of cells: " << obj.nCells() << "\n";
