5526b8ad0fdf98c1215839c0871546f64fa7519e
EdoPro98 <edoardo.proserpio@gmail.com>
2022-05-10 00:04:09 +0200

    [Xcode] Xcode prefers static_cast instead of pybind::overload_cast

--- python/SiPMRandomPy.cpp
+++ python/SiPMRandomPy.cpp
@@ -1,6 +1,8 @@
 #include "SiPMRandom.h"
+#include <cstdint>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <vector>
 
 namespace py = pybind11;
 using namespace sipm;
@@ -9,14 +11,14 @@ void SiPMRandomPy(py::module& m) {
   py::class_<SiPMRandom> SiPMRandom(m, "SiPMRandom");
   SiPMRandom.def(py::init<>())
     .def("Rand", static_cast<double (SiPMRandom::*)(void)>(&SiPMRandom::Rand))
-    .def("randInteger", py::overload_cast<const uint32_t>(&SiPMRandom::randInteger))
+    .def("randInteger", static_cast<uint32_t (SiPMRandom::*)(const uint32_t)>(&SiPMRandom::randInteger))
     .def("randGaussian", static_cast<double (SiPMRandom::*)(const double, const double)>(&SiPMRandom::randGaussian))
     .def("randExponential", static_cast<double (SiPMRandom::*)(double)>(&SiPMRandom::randExponential))
     .def("randPoisson", &SiPMRandom::randPoisson)
     .def("Rand", static_cast<std::vector<double> (SiPMRandom::*)(const uint32_t)>(&SiPMRandom::Rand))
     .def("randGaussian", static_cast<std::vector<double> (SiPMRandom::*)(const double, const double, const uint32_t)>(
                            &SiPMRandom::randGaussian))
-    .def("randInteger", py::overload_cast<const uint32_t, const uint32_t>(&SiPMRandom::randInteger))
+    .def("randInteger", static_cast<std::vector<uint32_t>(SiPMRandom::*)(const uint32_t, const uint32_t)>(&SiPMRandom::randInteger))
     .def("randExponential",
          static_cast<std::vector<double> (SiPMRandom::*)(const double, const uint32_t)>(&SiPMRandom::randExponential));
 }
