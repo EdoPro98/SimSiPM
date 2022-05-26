e3d406aff33324bd749b9b3c5b961a00c36fc4e9
EdoPro98 <edoardo.proserpio@gmail.com>
2022-05-26 18:14:35 +0200

    [Tests] Renewed tests and better CMakeList

--- tests/xorshift.cpp
+++ tests/xorshift.cpp
@@ -7,34 +7,38 @@ using namespace sipm;
 
 struct TestSiPMXorshift256 : public ::testing::Test {
   static constexpr int N = 1000000;
-  SiPMRng::Xorshift256plus sut;
 };
 
 TEST_F(TestSiPMXorshift256, Constructor) {
-  uint64_t random = sut(); // Generate random value
+  uint64_t N = 10000000;
 }
 
 TEST_F(TestSiPMXorshift256, Seed) {
+  sipm::SiPMRng::Xorshift256plus rng;
   static constexpr uint64_t seed = 1234567890UL; // Random seed
   static constexpr uint64_t expected[] = {
     2408737001010924969ULL, 542980830696407883ULL, 6216539985890296057ULL, 9937196552999892625ULL, 14557266945446803870ULL,
     14778737618991270199ULL, 8040724028886114342ULL, 9480989170591651270ULL, 11279768352260025975ULL, 17595622770473001624ULL};
   for (int i = 0; i < N; ++i) {
-    sut.seed(seed);
+    rng.seed(seed);
     for (int j = 0; j < 10; ++j) {
-      uint64_t x = sut();
+      uint64_t x = rng();
       EXPECT_EQ(x, expected[j]) << ">> Some error in random generator";
     }
   }
 }
 
 TEST_F(TestSiPMXorshift256, AutomaticSeed) {
+  sipm::SiPMRng::Xorshift256plus rng1;
+  sipm::SiPMRng::Xorshift256plus rng2;
   for (int i = 0; i < 1000; ++i) {
-    sut.seed();              // Set automatic seed
-    uint64_t first = sut();  // Generate random value
-    sut.seed();              // Set automatic seed
-    uint64_t second = sut(); // Generate random value
-    EXPECT_NE(first, second) << ">> Some error in automatic seed generation";
+    rng1.seed();
+    rng2.seed();
+    for (int j = 0; j < 1000; ++j){
+      const uint64_t first = rng1();  // Generate random value
+      const uint64_t second = rng2(); // Generate random value
+      EXPECT_NE(first, second) << ">> Some error in automatic seed generation";
+    }
   }
 }
 
@@ -43,15 +47,17 @@ TEST_F(TestSiPMXorshift256, GenerationSmallWindowTest) {
   uint64_t first_run[n];
   uint64_t second_run[n];
 
+  sipm::SiPMRng::Xorshift256plus rng;
+
   for (int t = 0; t < N; ++t) {
     const uint64_t seed = rand(); // Generate a random seed
-    sut.seed(seed);               // Set seed
+    rng.seed(seed);               // Set seed
     for (int i = 0; i < n; ++i) {
-      first_run[i] = sut();
+      first_run[i] = rng();
     }
-    sut.seed(seed); // Set same seed
+    rng.seed(seed);               // Set same seed
     for (int i = 0; i < n; ++i) {
-      second_run[i] = sut();
+      second_run[i] = rng();
     }
     for (int i = 0; i < n; ++i) {
       EXPECT_EQ(first_run[i], second_run[i]) << ">> Generator with same seed produces different values";
@@ -67,16 +73,17 @@ TEST_F(TestSiPMXorshift256, GenerationMediumWindowTest) {
   static constexpr int n = 256;
   uint64_t first_run[n];
   uint64_t second_run[n];
+  sipm::SiPMRng::Xorshift256plus rng;
 
   for (int t = 0; t < N; ++t) {
     const uint64_t seed = rand(); // Generate a random seed
-    sut.seed(seed);               // Set seed
+    rng.seed(seed);               // Set seed
     for (int i = 0; i < n; ++i) {
-      first_run[i] = sut();
+      first_run[i] = rng();
     }
-    sut.seed(seed); // Set same seed
+    rng.seed(seed); // Set same seed
     for (int i = 0; i < n; ++i) {
-      second_run[i] = sut();
+      second_run[i] = rng();
     }
     for (int i = 0; i < n; ++i) {
       EXPECT_EQ(first_run[i], second_run[i]);
@@ -92,16 +99,16 @@ TEST_F(TestSiPMXorshift256, GenerationBigWindowTest) {
   static constexpr int n = 65536;
   uint64_t first_run[n];
   uint64_t second_run[n];
-
+  sipm::SiPMRng::Xorshift256plus rng;
   for (int t = 0; t < 1000; ++t) {
     const uint64_t seed = rand(); // Generate a random seed
-    sut.seed(seed);               // Set seed
+    rng.seed(seed);               // Set seed
     for (int i = 0; i < n; ++i) {
-      first_run[i] = sut();
+      first_run[i] = rng();
     }
-    sut.seed(seed); // Set same seed
+    rng.seed(seed); // Set same seed
     for (int i = 0; i < n; ++i) {
-      second_run[i] = sut();
+      second_run[i] = rng();
     }
     for (int i = 0; i < n; ++i) {
       EXPECT_EQ(first_run[i], second_run[i]); // Check that values are equal
@@ -115,9 +122,10 @@ TEST_F(TestSiPMXorshift256, GenerationBigWindowTest) {
 
 TEST_F(TestSiPMXorshift256, ShennonEntropy){
   std::map<uint8_t, uint64_t> counts;
+  sipm::SiPMRng::Xorshift256plus rng;
   for(int i=0; i<N; ++i){
     alignas(64) uint8_t bytes[8];
-    const uint64_t ull = sut();
+    const uint64_t ull = rng();
     std::memcpy(&bytes, &ull, sizeof(uint64_t));
     for(int j=0; j<8; ++j){
       counts[bytes[j]] += 1;
